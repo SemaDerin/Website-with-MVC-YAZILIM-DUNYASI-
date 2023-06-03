e019fa12bcad9ffc04c81fa61421b5365a329f0c
semaderin <derinnsema@gmail.com>
2023-06-03 16:01:34 +0300

    Proje dosyası ekle.

---
+++ MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Anasayfa = "active";
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            ViewBag.Iletisim = "active";
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.About = "active";
+            return View();
+        }
+        public ActionResult Hizmetler()
+        {
+            ViewBag.Hizmetler = "active";
+            return View();
+        }
+
+
+        public ActionResult Fiyatlandirma()
+        {
+            ViewBag.Fiyatlandirma = "active";
+            return View();
+        }
+
+
+    }
+}
